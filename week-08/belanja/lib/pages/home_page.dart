c370485eeeffd5f1e6d01bebc61012cdafeaee48
maidyjoshi <maidyputri13@gmail.com>
2023-10-24 01:18:46 +0700

    up week-08

---
+++ week-08/belanja/lib/pages/home_page.dart
@@ -0,0 +1,11 @@
+import 'package:belanja/pages/item_page.dart';
+import 'package:flutter/material.dart';
+import 'dart:html';
+
+class HomePage extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        // TODO: implement build
+        throw UnimplementError();
+    }
+}
