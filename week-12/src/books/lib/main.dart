3c6c6327ae3d903e92877ad0574187b49c2cb4ba
maidyjoshi <maidyputri13@gmail.com>
2023-11-19 17:16:31 +0700

    commit- prak2 no.4

--- week-12/src/books/lib/main.dart
+++ week-12/src/books/lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -18,7 +17,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Maidy Putri Joshi',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -35,49 +33,70 @@ class FuturePage extends StatefulWidget {
 }
 
 class _FuturePageState extends State<FuturePage> {
+  Future<int> returnOneAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 1;
+  }
+
+  Future<int> returnTwoAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 2;
+  }
+
+  Future<int> returnThreeAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 3;
+  }
+  Future count() async{
+    int total = 0;
+    total = await returnOneAsync();
+    total += await returnTwoAsync();
+    total += await returnThreeAsync();
+    setState(() {
+      result = total.toString();
+    });
+  }
+
   String result = '';
 
-      Future<Response> getData() async{
-      const authority= 'www.googleapis.com';
-      const path = '/books/v1/volumes/EpMFEAAAQBAJ';
-      Uri url = Uri.https(authority, path);
-      return http.get(url);
-    }
+  Future<Response> getData() async {
+    const authority = 'www.googleapis.com';
+    const path = '/books/v1/volumes/EpMFEAAAQBAJ';
+    Uri url = Uri.https(authority, path);
+    return http.get(url);
+  }
 
   @override
   Widget build(BuildContext context) {
- 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Back from the Future'),
       ),
       body: Center(
-        child: Column(
-          children: [
-            const Spacer(),
-            ElevatedButton(
-              child: const Text('GO!'),
-              onPressed: () {
-                setState(() {
-                  getData()
-                  .then((value) {
-                    result = value.body.toString().substring(0,450);
-                    setState(() {});
-                  }).catchError((_){
-                    result = 'An error occurred';
-                    setState(() {
-                    });
-                  });
+        child: Column(children: [
+          const Spacer(),
+          ElevatedButton(
+            child: Text('GO!'),
+            onPressed: () {
+              count();
+              setState(() {
+                getData().then((value) {
+                  result = value.body.toString().substring(0, 450);
+                  setState(() {});
+                }).catchError((_) {
+                  result = 'An error occurred';
+                  setState(() {});
                 });
-              }, 
-            ),            
+              });
+            },
+          ),
           const Spacer(),
           Text(result),
           const Spacer(),
           const CircularProgressIndicator(),
           const Spacer(),
-          ]),
+        ]),
       ),
     );
   }
