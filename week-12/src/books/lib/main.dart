bb560c09b868d16822120bf148e79294042537e1
maidyjoshi <maidyputri13@gmail.com>
2023-11-19 16:57:02 +0700

    commit prak 1 no. 1,2,3

---
+++ week-12/src/books/lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Maidy Putri Joshi',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const FuturePage(),
+    );
+  }
+}
+
+class FuturePage extends StatefulWidget {
+  const FuturePage({super.key});
+
+  @override
+  State<FuturePage> createState() => _FuturePageState();
+}
+
+class _FuturePageState extends State<FuturePage> {
+  String result = '';
+
+      Future<Response> getData() async{
+      const authority= 'www.googleapis.com';
+      const path = '/books/v1/volumes/EpMFEAAAQBAJ';
+      Uri url = Uri.https(authority, path);
+      return http.get(url);
+    }
+
+  @override
+  Widget build(BuildContext context) {
+ 
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Back from the Future'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            ElevatedButton(
+              child: const Text('GO!'),
+              onPressed: () {
+                setState(() {
+                  getData()
+                  .then((value) {
+                    result = value.body.toString().substring(0,450);
+                    setState(() {});
+                  }).catchError((_){
+                    result = 'An error occurred';
+                    setState(() {
+                    });
+                  });
+                });
+              }, 
+            ),            
+          const Spacer(),
+          Text(result),
+          const Spacer(),
+          const CircularProgressIndicator(),
+          const Spacer(),
+          ]),
+      ),
+    );
+  }
+}
